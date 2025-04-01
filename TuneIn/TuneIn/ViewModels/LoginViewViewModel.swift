37654db7bdb24c70890a5b6458a2cc8c25140fc8
jessicayingg <jessicaxying@gmail.com>
2025-04-01 01:50:45 -0400

    Adding view models for file clarity

---
+++ TuneIn/TuneIn/ViewModels/LoginViewViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewViewModel.swift
+//  TuneIn
+//
+//  Created by Jessica Ying on 2025-04-01.
+//
+
+import Foundation
+import SwiftUI
+
+class LoginViewViewModel: ObservableObject {
+    // Stores access token after user logs in
+    //@State private var accessToken: String? = nil
+    @Published var accessToken: String = ""
+    
+    let clientID = APIConstants.clientId
+    let redirectURI = APIConstants.redirectUri
+    
+    func loginWithSpotify() {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = APIConstants.authHost
+        components.path = "/authorize"
+        
+        components.queryItems = APIConstants.authParams.map { key, value in
+            URLQueryItem(name: key, value: value)
+        }
+        
+        if let url = components.url {
+            // Open url in Safari
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func handleSpotifyCallback(_ url: URL) {
+        // Spotify will redirect with a fragment (e.g., tunein://callback#access_token=YOUR_ACCESS_TOKEN)
+        guard let fragment = url.fragment else { return }
+        
+        // Split the fragment into key-value pairs
+        let params = fragment.components(separatedBy: "&").reduce(into: [String: String]()) { result, param in
+            let parts = param.components(separatedBy: "=")
+            if parts.count == 2 {
+                result[parts[0]] = parts[1]
+            }
+        }
+        
+        // Extract the access token
+        if let token = params["access_token"] {
+            accessToken = token  // Update the state with the access token
+        }
+    }
+}
